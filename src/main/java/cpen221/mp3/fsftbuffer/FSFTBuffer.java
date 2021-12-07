d7a654d81b9c429a67ffcd6a380f2cfaecb12228
rkna02 <71546323+rkna02@users.noreply.github.com>
2021-12-07 11:43:24 -0800

    Update FSFTBuffer.java

--- src/main/java/cpen221/mp3/fsftbuffer/FSFTBuffer.java
+++ src/main/java/cpen221/mp3/fsftbuffer/FSFTBuffer.java
@@ -1,5 +1,9 @@
 package cpen221.mp3.fsftbuffer;
+import java.util.*;
+import java.util.Collections;
 
+/** FSFTBuffer represents a finite-state finite-time buffer
+ *  which stores a finite amount of objects for a finite amount of time */
 public class FSFTBuffer<T extends Bufferable> {
 
     /* the default buffer size is 32 objects */
@@ -8,7 +12,62 @@ public class FSFTBuffer<T extends Bufferable> {
     /* the default timeout value is 3600s */
     public static final int DTIMEOUT = 3600;
 
-    /* TODO: Implement this datatype */
+    /** Concrete Representation */
+    /** Two integers to specify the capacity and timeout of the buffer */
+    private final int capacity;
+    private final int timeout;
+
+    /** Synchronized list that stores objects of type T */
+    private final List<T> list = Collections.synchronizedList(new ArrayList<>());
+
+    /** Synchronized map that stores object ids and object expiry times */
+    private final Map <String, Long> map = Collections.synchronizedMap(new HashMap<>());
+
+    //Representation Invariant:
+    //  list != null
+    //  map != null
+
+    //Abstract Function:
+    //  Buffer's maximum number of stored objects and expiry time is
+    //  represented by capacity and timeout respectively
+    //  capacity = N represents that the buffer can hold a maximum of N objects
+    //  timeout = M represents that the buffer will expire after M seconds
+    //  Example:
+    //  capacity = 5 means the buffer can hold a maximum of 5 objects
+    //  timeout = 3600 means the buffer will expire after 3600 seconds (1 hour)
+    //
+    //  Storage for objects is represented by a synchronized list
+    //  list.get(N) represents an object of type T that is in the buffer
+    //  Please note that this object may be a stale object since stale objects
+    //  are not removed immediately after expiry
+    //
+    //  Object details are represented by a map,
+    //  object ids as key and object expiry times as value
+    //  map<N, M> represents that the object id is N and the object expiry time is at M seconds
+    //  Please note that this object may be a stale object since stale objects
+    //  are not removed immediately after expiry
+
+    //Thread Safety Argument:
+    //  capacity and timeout are final, so those variables are immutable and thread-safe
+    //  list and map are final, so those variables are immutable and thread-safe
+    //  list and map points to thread safe datatypes
+
+
+    /**
+     * Checks the representation invariant.
+     *
+     * effects: no effects if this satisfies rep invariant,
+     * 		    otherwise throws a runtime exception.
+     */
+    private void checkRep() {
+        if (list == null) {
+            throw new RuntimeException("ERROR: instantiated list is null");
+        }
+        if (map == null) {
+            throw new RuntimeException("ERROR: instantiated map is null");
+        }
+    }
+
 
     /**
      * Create a buffer with a fixed capacity and a timeout value.
@@ -20,11 +79,15 @@ public class FSFTBuffer<T extends Bufferable> {
      *                 be in the buffer before it times out
      */
     public FSFTBuffer(int capacity, int timeout) {
-        // TODO: implement this constructor
+        this.capacity = capacity;
+        this.timeout = timeout;
     }
+
 
     /**
      * Create a buffer with default capacity and timeout values.
+     * Objects in the buffer that have not been refreshed within the
+     * timeout period are removed from the cache.
      */
     public FSFTBuffer() {
         this(DSIZE, DTIMEOUT);
