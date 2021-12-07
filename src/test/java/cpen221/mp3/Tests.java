ac4f7d024309924bf413241ac90e59e2bf5ba4d2
rkna02 <71546323+rkna02@users.noreply.github.com>
2021-12-07 15:26:02 -0800

    Update Tests.java

--- src/test/java/cpen221/mp3/Tests.java
+++ src/test/java/cpen221/mp3/Tests.java
@@ -1,11 +1,34 @@
 package cpen221.mp3;
+import cpen221.mp3.fsftbuffer.Bufferable;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import cpen221.mp3.fsftbuffer.FSFTBuffer;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class Tests {
 
-    /*
-        You can add your tests here.
-        Remember to import the packages that you need, such
-        as cpen221.mp3.fsftbuffer.
-     */
+    private static FSFTBuffer<Bufferable> buffer1;
+    private static FSFTBuffer<Bufferable> buffer2;
+    private static FSFTBuffer<Bufferable> buffer3;
+    private static FSFTBuffer<Bufferable> buffer4;
+    private static FSFTBuffer<Bufferable> buffer5;
+
+    @BeforeAll
+    public static void setupTests() {
+        buffer1 = new FSFTBuffer<Bufferable>(20, 5);
+        buffer2 = new FSFTBuffer<Bufferable>(20, 5);
+        buffer3 = new FSFTBuffer<Bufferable>(20, 5);
+        buffer4 = new FSFTBuffer<Bufferable>(20, 5);
+        buffer5 = new FSFTBuffer<Bufferable>(1, 5);
+
+        /**
+         * Task 1 Tests
+         */
+
+    }
+
 
 }
